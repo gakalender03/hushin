cc3402837e4d9ae01e185f8579f17d7eade55953
free palestine <122424206+gakalender03@users.noreply.github.com>
2025-11-19 15:11:20 +0700

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ const PRIVATE_KEYS = process.env.PRIVATE_KEYS.split('\n')
 
 const networkConfig = {
   name: 'Pharos Testnet',
-  chainId: 688689,
+  chainId: 688688,
   rpcUrl: RPC_URL,
   currencySymbol: 'PHRS',
 };
